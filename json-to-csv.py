30bed4d39b9931f8cbad297cb16cc5bb455ddc9f
drewbranson <37157743+drewbranson@users.noreply.github.com>
2023-09-12 18:16:19 -0700

    update

--- json-to-csv.py
+++ json-to-csv.py
@@ -96,7 +96,7 @@ for tile in pixels.keys():
         g = int(g)
         b = int(b)
         
-        if ((-254<x<254) and (-254<y<254) and (int(z)>=5)):
+        if ((-254<x<254) and (-254<y<254) and (int(z)>=6)):
             img.putpixel((x,y), (r,g,b))
             img.putpixel((x+1,y+1), (r,g,b))
             img.putpixel((x+1,y-1), (r,g,b))
