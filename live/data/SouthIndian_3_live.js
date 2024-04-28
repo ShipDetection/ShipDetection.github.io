148c8928be957574724d91239eab6cbc54b26f1c
drewbranson <37157743+drewbranson@users.noreply.github.com>
2024-04-28 01:37:13 -0700

    update

--- live/data/SouthIndian_3_live.js
+++ live/data/SouthIndian_3_live.js
@@ -1,4 +1,80 @@
 var json_SouthIndian_3 = {
   "type": "FeatureCollection",
-  "features": []
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "field_1": "0",
+        "Date": "20240427",
+        "DaysOld": "1.0"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          51.62387284423631,
+          10.221298933784372
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "field_1": "1",
+        "Date": "20240427",
+        "DaysOld": "1.0"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          51.56965909345583,
+          10.172102004924293
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "field_1": "2",
+        "Date": "20240427",
+        "DaysOld": "1.0"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          51.47516202720987,
+          10.138865946908895
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "field_1": "3",
+        "Date": "20240427",
+        "DaysOld": "1.0"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          51.52569230600086,
+          10.058930393726357
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "field_1": "4",
+        "Date": "20240427",
+        "DaysOld": "1.0"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          51.25670464931341,
+          9.300055123021489
+        ]
+      }
+    }
+  ]
 }
